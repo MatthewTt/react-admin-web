9a38544631fbf90afea485a6afb5a73e8a801f7f
liujiajie <1176020631@qq.com>
2023-10-21 20:39:06 +0800

    ci: update nginx

--- nginx/config.sh
+++ nginx/config.sh
@@ -28,6 +28,13 @@ server {
     root /data/www/;
     index index.html;
   }
+
+  location /api {
+      proxy_pass $SERVER_URL;
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
 }
 
 EOF
