d9da546b249e426103f8eafd9fad80f525485329
liujiajie <1176020631@qq.com>
2023-10-22 11:41:15 +0800

    ci: update nginx

--- nginx/config.sh
+++ nginx/config.sh
@@ -31,9 +31,9 @@ server {
 
   location /api {
       proxy_pass $SERVER_URL;
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Host \$host;
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 }
 
